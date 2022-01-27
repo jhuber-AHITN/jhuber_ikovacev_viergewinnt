4401c22e129af9afd12edaa942218bfd582c5a69
Julian Huber <75020137+jhuber-AHITN@users.noreply.github.com>
2022-01-27 16:34:24 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.viergewinntversuch5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.viergewinntversuch5 to javafx.fxml;
-    exports com.example.viergewinntversuch5;
-}
