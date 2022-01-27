18452f407addc005ff24876a97fbd128868ba324
jhuber <jhuber@htl-steyr.ac.at>
2022-01-27 16:50:26 +0100

    Lauffähig endlich

--- src/main/java/com/example/viergewinntv7/HelloApplication.java
+++ src/main/java/com/example/viergewinntv7/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("dummer hs!");
+        stage.setTitle("Wir haben es GESCHAFGASHUIabsipufbIOFIUSHIOPÜDT!");
         stage.setScene(scene);
         stage.show();
     }
