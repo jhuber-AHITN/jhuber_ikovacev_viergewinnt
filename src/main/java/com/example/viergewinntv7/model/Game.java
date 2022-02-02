41a9166b3ee22742e7dd7f1ff8c4c9eb5f71e5f6
jhuber <jhuber@htl-steyr.ac.at>
2022-02-02 15:46:16 +0100

    Hope it works

--- src/main/java/com/example/viergewinntv7/model/Game.java
+++ src/main/java/com/example/viergewinntv7/model/Game.java
@@ -1,38 +1,39 @@
 package com.example.viergewinntv7.model;
 
-public class Game {
-    protected Player playerUno;
-    protected Player playerDos;
-    protected Playfield playfield;
+import com.example.viergewinntv7.controller.ControllerConsole;
 
-    public Game(Player playerUno, Player playerDos, Playfield playfield) {
-        this.playerUno = playerUno;
-        this.playerDos = playerDos;
+public class Game {
+    protected Player[] players  = new Player[2];
+    protected Player currentPLayer;
+    protected Playfield playfield;
+    protected int[] lastset = new int[2];
+
+    public Game(Player playerOne, Player playerTwo, Playfield playfield) {
+        players[0] = playerOne;
+        players[1] = playerTwo;
         this.playfield = playfield;
     }
 
-    public Game() {
+    public void set(int col){
+        playfield.setcell(playfield.checkcol(col), col);
+        lastset[1] = col;
+        lastset[0] = playfield.checkcol(col);
     }
 
-    public void setPlayerUno(Player playerUno) {
-        this.playerUno = playerUno;
+    public void undo(){
+        playfield.unsetcell(lastset[0], lastset[1]);
     }
 
-    public void setPlayerDos(Player playerDos) {
-        this.playerDos = playerDos;
+    public void reset(){
+        playfield.clear();
+        ControllerConsole.Initialize();
     }
+
 
     public void setPlayfield(Playfield playfield) {
         this.playfield = playfield;
     }
 
-    public Player getPlayerUno() {
-        return playerUno;
-    }
-
-    public Player getPlayerDos() {
-        return playerDos;
-    }
 
     public Playfield getPlayfield() {
         return playfield;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.viergewinntv7 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.viergewinntv7 to javafx.fxml;
