74d19f26df275d69b178c4ac3f905f22f2f4beb6
Ismael Kovacevic <ikovacev@htl-steyr.ac.at>
2022-02-11 10:50:45 +0100

    Added  playfield.fxml updated hello-view.fxml

--- src/main/java/com/example/viergewinntv7/HelloController.java
+++ src/main/java/com/example/viergewinntv7/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.viergewinntv7;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private HBox HboxPlaying;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    protected void CreatePlayField(ActionEvent event) throws IOException {
+        URL u = getClass().getResource("playfield.fxml");
+        Pane newLoadedPane =        FXMLLoader.load(u);
+        HboxPlaying.getChildren().add(newLoadedPane);
     }
 }
