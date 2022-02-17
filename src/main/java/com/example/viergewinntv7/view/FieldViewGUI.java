8edebb55798479bebc504eff0523cec27e2b1ece
jhuber <jhuber@htl-steyr.ac.at>
2022-02-17 12:26:05 +0100

    PlayfieldView angelegt

---
+++ src/main/java/com/example/viergewinntv7/view/FieldViewGUI.java
@@ -0,0 +1,12 @@
+package com.example.viergewinntv7.view;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class FieldViewGUI {
+    static public void UpdateCircle(Color color, Circle circle){
+        circle.setFill(color);
+    }
+
+
+}
